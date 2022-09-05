ed868ca9a22b009949fa16f6981b1835ec5af035
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-05 03:07:45 +0300

    reset boundary function

--- game.js
+++ game.js
@@ -18,5 +18,31 @@ window.addEventListener("load", function () {
     maze = this.document.getElementById("game")
 
 
-   
+    // Reset function for the start div
+    function reset() {
+        score = 0
+
+        boundaries.forEach(boundary => {
+            boundary.setAttribute('style', 'background-color: #eeeeee;');
+            status.innerHTML = 'Begin by moving your mouse over the "S".'
+        });
+        var maze_box = document.getElementById('game');
+
+        document.addEventListener('mouseover', function (event) {
+            if (!maze_box.contains(event.target)) {
+                lost()
+            }
+        });
+    }
+    // boundary function when the S is clicked
+    function boundary() {
+        boundaries.forEach(boundary => {
+            boundary.addEventListener('mouseover', function handleClick(event) {
+                console.log('box clicked', event);
+                lost()
+            });
+        });
+    }
+    
+    
 })
